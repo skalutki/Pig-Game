453c0606526b68f8df5b15af6d5fe95645fc851b
skalutki <skalutki@gmail.com>
2021-12-01 05:18:31 +0000

    Feat: Switch player when dice is 1, reset current score.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -17,7 +19,9 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 // Curent score
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Roling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -35,8 +39,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add dice to the current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LETER FIXME
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
